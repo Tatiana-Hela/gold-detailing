53bc0aa1955cbc252f317969b2f1ee868172e6b7
Tatiana-Hela <malya9472@gmail.com>
2023-07-06 22:23:20 +0300

    finish slider

---
+++ src/js/slider-car.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const sliderItems = document.querySelectorAll('.slider-car__item');
+  const defaultActiveIndex = 4;
+
+  sliderItems.forEach(function (item, index) {
+    if (index === defaultActiveIndex) {
+      item.classList.add('active');
+    }
+  });
+
+  let activeIndex = defaultActiveIndex;
+
+  sliderItems.forEach(function (item, index) {
+    item.addEventListener('mouseover', function () {
+      setActiveSlide(index);
+    });
+
+    item.addEventListener('focus', function () {
+      setActiveSlide(index);
+    });
+  });
+
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowLeft') {
+      activeIndex = (activeIndex - 1 + sliderItems.length) % sliderItems.length;
+      setActiveSlide(activeIndex);
+    }
+
+    if (event.key === 'ArrowRight') {
+      activeIndex = (activeIndex + 1) % sliderItems.length;
+      setActiveSlide(activeIndex);
+    }
+  });
+
+  function setActiveSlide(index) {
+    sliderItems.forEach(function (item) {
+      item.classList.remove('active');
+    });
+
+    sliderItems[index].classList.add('active');
+    activeIndex = index;
+  }
+});
