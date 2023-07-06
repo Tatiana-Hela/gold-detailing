640a45f2520f9becd720b86baa114b1c5f1a664d
Tatiana-Hela <malya9472@gmail.com>
2023-07-06 22:29:28 +0300

    fix btn

--- src/js/active-btn.js
+++
@@ -1,7 +0,0 @@
-function toggleActiveState(button) {
-  button.classList.add('active'); // Добавляем класс "active" на кнопку
-
-  setTimeout(function () {
-    button.classList.remove('active'); // Удаляем класс "active" с кнопки
-  }, 1000); // 1000 миллисекунд = 1 секунда
-}
