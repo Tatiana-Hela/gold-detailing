6c8b3ebb7f84f3f1d0aca407b703a3cb7770751b
Tatiana-Hela <malya9472@gmail.com>
2023-07-08 01:14:03 +0300

    gallery

--- src/js/gallery.js
+++
@@ -1,8 +0,0 @@
-const fullscreenPhoto = document.querySelector('.gallery__fullscreen');
-const galleryPhotos = document.querySelectorAll('.gallery__photo');
-
-galleryPhotos.forEach(photo => {
-  photo.addEventListener('click', function () {
-    fullscreenPhoto.src = photo.src;
-  });
-});
