734f86717ed435dd177b21f0914bfed3fab03449
Tatiana-Hela <tetianahela@gmail.com>
2023-07-09 12:00:45 +0000

    Deploying to gh-pages from @ Tatiana-Hela/gold-detailing@54f6bc643026cc58d297d1c15e6c1508197244e7 🚀

---
+++ index.f7f604a3.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".btn-burger"),n=document.querySelector(".menu-overlay"),t=document.querySelector(".btn-close");e.addEventListener("click",function(){n.style.display="block"}),t.addEventListener("click",function(){n.style.display="none"})});
+//# sourceMappingURL=index.f7f604a3.js.map
--- index.fe2be784.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".btn-burger"),n=document.querySelector(".menu-overlay"),t=document.querySelector(".btn-close");e.addEventListener("click",(function(){n.style.display="block"})),t.addEventListener("click",(function(){n.style.display="none"}))}));
-//# sourceMappingURL=index.fe2be784.js.map
