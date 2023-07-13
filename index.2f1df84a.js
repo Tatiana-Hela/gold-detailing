f3e0cbd4b399c2dd46c0e8b259f82217bd886b20
Tatiana-Hela <tetianahela@gmail.com>
2023-07-13 12:59:52 +0000

    Deploying to gh-pages from @ Tatiana-Hela/gold-detailing@80654b895ba47693bc494205c2e4c3dc4b2b3010 🚀

---
+++ index.2f1df84a.js
@@ -0,0 +1,2 @@
+var e=L.map("map").setView([50.43186,30.359328],13);L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{maxZoom:18,titleSize:512}).addTo(e),L.marker([50.43186,30.359328]).addTo(e);
+//# sourceMappingURL=index.2f1df84a.js.map
