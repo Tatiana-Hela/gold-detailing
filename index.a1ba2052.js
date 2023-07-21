664fb83bfdfd60effaba97035a2d694d7edc527f
Tatiana-Hela <tetianahela@gmail.com>
2023-07-21 14:16:07 +0000

    Deploying to gh-pages from @ Tatiana-Hela/gold-detailing@77a337a6ab9b43fb6f17781ebf62ea71b2d0552a 🚀

---
+++ index.a1ba2052.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".accordion-item");e.forEach(e=>{let c=e.querySelector(".accordion-header"),t=e.querySelector(".accordion-content"),o=document.querySelector(".accordion-item");c.addEventListener("click",()=>{o.classList.toggle("active"),o.classList.contains("active")?t.style.display="block":t.style.display="none"})});
+//# sourceMappingURL=index.a1ba2052.js.map
