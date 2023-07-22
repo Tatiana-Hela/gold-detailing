536311099898644480512a8755cc1c172cd78bef
Tatiana-Hela <tetianahela@gmail.com>
2023-07-22 14:48:22 +0000

    Deploying to gh-pages from @ Tatiana-Hela/gold-detailing@07f00088e0a5133d582a36c9905ff7f766598f22 🚀

---
+++ index.d3ac9f2f.js
@@ -0,0 +1,2 @@
+!function(){let e=document.querySelectorAll(".accordion-item");e.forEach(t=>{let c=t.querySelector(".accordion-header"),o=t.querySelector(".accordion-content");c.addEventListener("click",()=>{e.forEach(e=>{e!==t&&(e.classList.remove("active"),e.querySelector(".accordion-content").style.height="0")}),t.classList.toggle("active"),t.classList.contains("active")?o.style.height=o.scrollHeight+"px":o.style.height="0"})})}();
+//# sourceMappingURL=index.d3ac9f2f.js.map
