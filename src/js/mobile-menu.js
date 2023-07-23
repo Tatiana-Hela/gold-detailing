cb4f7e80e90e02b04e482eee74d4df398249c72e
Tatiana-Hela <malya9472@gmail.com>
2023-07-23 09:45:07 +0300

    Update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
   var btnBurger = document.querySelector('.btn-burger');
+  var btnClose = document.querySelector('.btn-close');
   var menuOverlay = document.querySelector('.menu-overlay');
-  var menuItems = document.querySelectorAll('.mobile-menu-list-item'); // Получаем все пункты меню
+  var menuItems = document.querySelectorAll('.mobile-menu-list-item');
 
   btnBurger.addEventListener('click', function () {
     menuOverlay.style.display = 'block';
   });
 
+  btnClose.addEventListener('click', function () {
+    menuOverlay.style.display = 'none';
+  });
+
   menuItems.forEach(function (menuItem) {
     menuItem.addEventListener('click', function () {
-      menuOverlay.style.display = 'none'; // Скрываем меню при выборе пункта
+      menuOverlay.style.display = 'none';
     });
   });
 });
