3810ff5ea30aafacaa62357cbb5142f7f8bf8970
Tatiana-Hela <malya9472@gmail.com>
2023-07-24 17:05:59 +0300

    add js

---
+++ src/detailing.js
@@ -0,0 +1,9 @@
+import './js/button-up';
+import './js/footer-map';
+import './js/gallery-slider';
+import './js/mobile-menu';
+import './js/reviews-slider';
+import './js/scroll';
+import './js/services';
+import './js/slider-car';
+import './js/detailing-services';
