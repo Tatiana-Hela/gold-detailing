259dc531f51adc1939026dfc88000746b0603670
Tatiana-Hela <malya9472@gmail.com>
2023-08-13 14:57:42 +0300

    finish

--- src/js/footer-map.js
+++ src/js/footer-map.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([50.43186, 30.359328], 13); // Установка центра карты на Киеве
+var map = L.map('map').setView([50.426907, 30.361476], 13); // Установка центра карты на Киеве
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
   titleSize: 512,
 }).addTo(map);
 
 // Добавление маркера с адресом
-var marker = L.marker([50.43186, 30.359328]).addTo(map);
+var marker = L.marker([50.426907, 30.361476]).addTo(map);
