839074268abf4426cd7c4613a6923c5fcf09bca1
Artem Sdobnikov <artem.sdobnikov@1worldonline.com>
2024-03-19 20:33:43 +0200

    remove unnecessary files, change text and colors

--- src/js/footer-map.js
+++ src/js/footer-map.js
@@ -1,8 +1,15 @@
-var map = L.map('map').setView([50.426907, 30.361476], 13); // Установка центра карты на Киеве
+const coordinates = [
+  50.40264008081105,
+  30.40875833161278,
+];
+
+// Установка центра карты на Киеве
+const map = L.map('map').setView(coordinates, 13);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
   titleSize: 512,
 }).addTo(map);
 
 // Добавление маркера с адресом
-var marker = L.marker([50.426907, 30.361476]).addTo(map);
+L.marker(coordinates).addTo(map);
